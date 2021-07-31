0e7811d5ab1f9c6704504a9cb4988f403bc55047
Partho Das <parthodas6176@gmail.com>
2021-07-31 17:27:54 +0530

    added validation to user's input

--- main.py
+++ main.py
@@ -1,9 +1,41 @@
+import string
 import os
+
+def is_valid_project_name(project_name):
+
+    LEGAL_PROJECT_NAME_CHARS = (
+    list(string.ascii_lowercase) +
+    list(string.ascii_uppercase) +
+    [str(x) for x in range(0, 10)] +
+    ['-', '_'])
+
+    for ch in project_name:
+        if ch not in LEGAL_PROJECT_NAME_CHARS:
+            return False
+
+    if len(project_name)<2 or len(project_name)>20:
+        return False
+
+    return True
+
+def is_valid_author_name(author_name):
+    return len(author_name) > 0
 
 def ask_input():
     parent_dir = input('Parent directory: ')
-    name = input('Name of the project: ')
-    author = input('Author: ')
+
+    while True:
+        name = input('Name of the project: ')
+        if is_valid_project_name(name):
+            break
+        print('Invalid Project Name! Try Again!')
+
+    while True:
+        author = input('Author: ')
+        if is_valid_author_name(author):
+            break
+        print('Invalid Author Name! Try Again!')
+        
     return parent_dir, name, author
 
 def make_dir(parent_dir, name):
@@ -16,8 +48,7 @@ def createREADME(parent_dir, name, author):
     path = parent_dir+'\README.md'
     README = open(path , 'w')
     
-    content = f"""Welcome to {name}
-Created by {author}"""
+    content = f"Welcome to {name}\nCreated by {author}"
     
     README.write(content)
     README.close()
@@ -27,10 +58,9 @@ def createMain(parent_dir, name):
     path = parent_dir+'\main.py'
     main_file = open(path, 'w')
     
-    boilerplate=f"""if __name__ == '__main__':
-    print('Welcome to {name}')"""
+    content = f"if __name__ == '__main__':\n\tprint('Welcome to {name}')"
                 
-    main_file.write(boilerplate)
+    main_file.write(content)
     main_file.close()
     print(f'Created file {path}')
 
