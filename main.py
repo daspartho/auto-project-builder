80e3a3533c5e611decbf6a521aee78634c630980
Partho Das <parthodas6176@gmail.com>
2021-07-31 14:41:21 +0530

    creates README

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 import os
 
-parent_dir = input('Parent directory: ')
-name = input('Name of the project: ')
-author = input('Author: ')
+def ask_input():
+    parent_dir = input('Parent directory: ')
+    name = input('Name of the project: ')
+    author = input('Author: ')
+    return parent_dir, name, author
 
-path = os.path.join(parent_dir, name)
-os.mkdir(path)
+def make_dir(parent_dir, name):
+    path = os.path.join(parent_dir, name)
+    os.mkdir(path)
+    return path
+
+def CreateREADME(parent_dir, name, author):
+    README = open(parent_dir+'\README.md', 'w')
+    README.write(f'Name: {name}\nAuthor: {author}')
+    README.close()
+
+def main():
+    parent_dir, name, author = ask_input()
+    path = make_dir(parent_dir, name)
+    CreateREADME(path, name, author)
+
+if __name__ == '__main__':
+    main()
